8b937f460aea6905a8ce66fc8bfeecd1bdaf659e
z2lai <zhenglai3@gmail.com>
2020-12-13 14:15:42 -0500

    fixed canvas hover position on hover over hero text content

--- assets/scripts/App.js
+++ assets/scripts/App.js
@@ -1,8 +1,10 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
-import { particles } from './modules/particles';
+import { particles, ParticlesOverlay } from './modules/particles';
 import { config } from './modules/particlesjs-config';
 
 particles('particles-js', config);
-var mobileMenu = new MobileMenu();
-var revealOnScroll = new RevealOnScroll();
+const particlesOverlay = new ParticlesOverlay('particles-js', 'particles-js-overlay')
+const mobileMenu = new MobileMenu();
+const revealOnScroll = new RevealOnScroll();
+
