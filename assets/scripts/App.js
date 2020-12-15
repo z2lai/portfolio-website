56656c69387285e851dcbad6f1daa3746d555982
z2lai <zhenglai3@gmail.com>
2020-12-15 10:58:33 -0500

    added banner button and fade transition on window load

--- assets/scripts/App.js
+++ assets/scripts/App.js
@@ -3,8 +3,16 @@ import RevealOnScroll from './modules/RevealOnScroll';
 import { particles, ParticlesOverlay } from './modules/particles';
 import { config } from './modules/particlesjs-config';
 
-particles('particles-js', config);
-const particlesOverlay = new ParticlesOverlay('particles-js', 'particles-js-overlay')
+window.addEventListener('load', function(){
+  console.log('Window loaded!');
+  window.setTimeout(function() {
+    document.getElementById('banner').classList.remove('large-hero--is-preload');
+  }, 100);
+});
+
+particles('banner', config);
+const particlesOverlay = new ParticlesOverlay('banner', 'banner-overlay')
+const particlesOverlayButton = new ParticlesOverlay('banner', 'banner-overlay-button')
 const mobileMenu = new MobileMenu();
 const revealOnScroll = new RevealOnScroll();
 
